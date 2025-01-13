1a536729aa17aff7cb18d6b73b2e592427bd5858
Jiue-Ren Liao (Rick) <xrickliao@gmail.com>
2025-01-13 17:36:13 +0800

    update

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os
-from fastapi import FastAPI, Request, HTTPException
+from fastapi import FastAPI, Request, HTTPException,File, UploadFile
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, JSONResponse
 from app.ai_chat_service import AIChatService
 from dotenv import load_dotenv
+# from libs.ocr_content import *
 
 # 初始化 FastAPI 應用
 app = FastAPI()
@@ -46,11 +47,21 @@ async def get_chat_template(request: Request):
 @app.get("/chat-content", response_class=HTMLResponse)
 async def get_chat_content():
     try:
-        with open("templates/ai-chat-content.html", "r", encoding="utf-8") as file:
+        with open("templates/chat/ai-chat-content.html", "r", encoding="utf-8") as file:
             content = file.read()
         return HTMLResponse(content)
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Failed to load chat content: {str(e)}")
+    
+# OCR Content Generation
+@app.get("/ocr-page", response_class=HTMLResponse)
+async def get_ocr_content():
+    try:
+        with open("templates/ocr/ocr_page.html", "r", encoding="utf-8") as file:
+            content = file.read()
+        return HTMLResponse(content)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Failed to load ocr service page: {str(e)}")
 
 # AI 聊天接口（非流式）
 @app.post("/api/ai-chat")
@@ -62,7 +73,6 @@ async def api_ai_chat(request: Request):
         history = data.get("history", [])
         if not message:
             raise HTTPException(status_code=400, detail="Message is required")
-
         # 調用 AIChatService 的生成方法
         ai_response = await ai_chat_service.generate(message, history)
 
@@ -91,7 +101,23 @@ async def api_ai_chat_stream(request: Request):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+# 文件上傳 API
+@app.post("/upload_file")
+async def upload_file(file: UploadFile = File(...)):
+    try:
+        # 確保上傳目錄存在
+        upload_dir = "upload_tmp"
+        os.makedirs(upload_dir, exist_ok=True)
 
+        # 保存文件
+        file_path = os.path.join(upload_dir, file.filename)
+        with open(file_path, "wb") as f:
+            f.write(await file.read())
+
+        return JSONResponse(content={"message": "File uploaded successfully", "filename": file.filename}, status_code=200)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"File upload failed: {str(e)}")
+    
 # 啟動應用
 if __name__ == "__main__":
     import uvicorn
